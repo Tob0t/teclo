4fa07b3746f832fd4b42cd900958562b87795475
rramler <rudolf.ramler@scch.at>
2017-06-07 01:47:57 +0200

    Making example entries created in createNewBug more verbose

--- Teclo-00-Base/src/at/scch/teclo/tests/CreateNewBugTest.java
+++ Teclo-00-Base/src/at/scch/teclo/tests/CreateNewBugTest.java
@@ -3,6 +3,10 @@ package at.scch.teclo.tests;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import at.scch.teclo.BugzillaTest;
 import at.scch.teclo.pageobjects.CreateNewBugPage;
 import at.scch.teclo.pageobjects.LoggedInBasePage;
@@ -21,20 +25,27 @@ public class CreateNewBugTest extends BugzillaTest {
 
 	@Test
 	public void testCreateNewBugDefaultFields() throws Exception {
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS");
+		String summary = "ExampleBugDefault_" + dateFormat.format(new Date());
+		String comment = "This is an example description for ExampleBugDefault created at " + dateFormat.format(new Date());
+		
 		CreateNewBugPage createNewBugPage = loggedInBasePage.navigateToCreateNewBugPage();
-		createNewBugPage.fillSummary("ExampleBugDefault");
-		createNewBugPage.fillComment("This is an example description for ExampleBugDefault");
+		createNewBugPage.fillSummary(summary);
+		createNewBugPage.fillComment(comment);
+		
 		NewBugCreatedPage newBugCreatedPage = createNewBugPage.commitBug();
-
+		
 		// Check if creating bug was successful
-		assertEquals("Bug " + newBugCreatedPage.getBugID() + " has been added to the database", newBugCreatedPage.getBugWasAddedText());
+		int bugId = newBugCreatedPage.getBugID();
+		assertEquals("Bug " + bugId + " has been added to the database", newBugCreatedPage.getBugWasAddedText());
+		
+		System.out.println("Created new default bug with summary " + summary + " and ID " + bugId);
 		
 		// verify changes including default values
-		// TODO: verify default values according to operating system and platform
-		//assertEquals("PC", newBugCreatedPage.getCurrentPlatform());
-		//assertEquals("Windows", newBugCreatedPage.getCurrentOpSys());
+		// don't verify default values for operating system and platform, which are client values retrieved from browser by default
 		
-		assertEquals("ExampleBugDefault", newBugCreatedPage.getSummary());
+		assertEquals(summary, newBugCreatedPage.getSummary());
+		
 		assertEquals("P5", newBugCreatedPage.getCurrentPriority());
 		assertEquals("enhancement", newBugCreatedPage.getCurrentSeverity());
 		
@@ -46,21 +57,28 @@ public class CreateNewBugTest extends BugzillaTest {
 	
 	@Test
 	public void testCreateNewBugAdvancedFields() throws Exception {
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss_SSS");
+		String summary = "ExampleBugAdvanced_" + dateFormat.format(new Date());
+		String comment = "This is an example description for ExampleBugAdvanced created at " + dateFormat.format(new Date());
+		
 		CreateNewBugPage createNewBugPage = loggedInBasePage.navigateToCreateNewBugPage();
-		createNewBugPage.fillSummary("ExampleBugAdvanced");
+		createNewBugPage.fillSummary(summary);
+		createNewBugPage.fillComment(comment);
+		
 		createNewBugPage.fillPlatform("Other");
 		createNewBugPage.fillOpSys("Linux");
 		createNewBugPage.fillSeverity("major");
 		
-		createNewBugPage.fillComment("This is an example description for ExampleBugAdvanced");
-		
 		NewBugCreatedPage newBugCreatedPage = createNewBugPage.commitBug();
 
 		// Check if creating bug was successful
-		assertEquals("Bug " + newBugCreatedPage.getBugID() + " has been added to the database", newBugCreatedPage.getBugWasAddedText());
+		int bugId = newBugCreatedPage.getBugID();
+		assertEquals("Bug " + bugId + " has been added to the database", newBugCreatedPage.getBugWasAddedText());
+		
+		System.out.println("Created new adv.ced bug with summary " + summary + " and ID " + bugId);
 		
 		// verify changes
-		assertEquals("ExampleBugAdvanced", newBugCreatedPage.getSummary());
+		assertEquals(summary, newBugCreatedPage.getSummary());
 		assertEquals("Other", newBugCreatedPage.getCurrentPlatform());
 		assertEquals("Linux", newBugCreatedPage.getCurrentOpSys());
 		assertEquals("major", newBugCreatedPage.getCurrentSeverity());
