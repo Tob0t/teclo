e95e10a7d7d54bbb8e6ac840aca241ff8ca2e2cc
rramler <rudolf.ramler@scch.at>
2017-06-14 09:25:58 +0200

    Added main to AllTests

--- Teclo-01-Usestatuswhiteboard/src/main/java/at/scch/teclo/tests/AllTests.java
+++ Teclo-01-Usestatuswhiteboard/src/main/java/at/scch/teclo/tests/AllTests.java
@@ -1,5 +1,7 @@
 package at.scch.teclo.tests;
 
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -10,5 +12,10 @@ import at.scch.teclo.AbstractBugzillaTest;
 @SuiteClasses({ LoginLogoutTest.class, CreateNewBugTest.class, EditBugTest.class, ChangeBugStatusTest.class,
 		SearchQuickTest.class, SearchSpecificTest.class, SearchAdvancedTest.class, GenerateTabularReportTest.class })
 public class AllTests extends AbstractBugzillaTest { // NOSONAR
-
+	
+	public static void main(String[] args) {
+		JUnitCore junitCore = new JUnitCore();
+		junitCore.addListener(new TextListener(System.out));
+		junitCore.run(AllTests.class);
+	}
 }
