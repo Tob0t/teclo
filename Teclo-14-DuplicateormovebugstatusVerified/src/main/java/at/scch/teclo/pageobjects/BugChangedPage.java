e744f82ad5be9de02034219df48baa85f0607f89
Tobias Madner <tobias.madner@gmx.at>
2017-06-20 17:12:31 +0200

    added Teclo-14-DuplicateormovebugstatusVerified

---
+++ Teclo-14-DuplicateormovebugstatusVerified/src/main/java/at/scch/teclo/pageobjects/BugChangedPage.java
@@ -0,0 +1,36 @@
+package at.scch.teclo.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class BugChangedPage extends AbstractLoggedinBugzillaPage {
+
+	@FindBy(id = "title")
+	private WebElement bugTitle;	
+	
+	public BugChangedPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	@Override
+	protected boolean isMatchingPage() {
+		return getTitle().matches("Bug \\d+ processed");
+	}
+	
+
+	public int getChangedBugId() {
+		return Integer.parseInt(bugTitle.getText().replaceAll("[^0-9]", ""));
+	}
+
+	public EditBugPage gotoChangedBugPage() {
+		driver.findElement(By.linkText("bug " + getChangedBugId())).click();
+		return PageFactory.initElements(driver, EditBugPage.class);
+	}
+
+	public String getSuccessMsg() {
+		return driver.findElement(By.cssSelector("dt")).getText();
+	}
+}
